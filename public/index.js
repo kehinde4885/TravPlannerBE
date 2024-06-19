f9f04acdc291c0d86d6e9e352a33f6a60bd54052
Kehinde4885 <Kehindemalagu@gmail.com>
2024-06-19 22:18:41 +0100

    (feat: base interactivity with Popover API) (fix: Layout & Spacing)

---
+++ public/index.js
@@ -0,0 +1,13 @@
+console.log("I am ready");
+
+
+let bot = document.getElementById('button')
+
+
+bot.addEventListener('click', buttonClicked)
+
+
+function buttonClicked(e) {
+  console.log("skjnsknsk")
+  console.log(e.target)
+}
